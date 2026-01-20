2210ca4abfbfa0d1405b553272599810a1b659e5
PDPSnG007 <pdpsng@googlemail.com>
2026-01-20 13:38:44 +0530

    Update agent.py

--- backend/agent.py
+++ backend/agent.py
@@ -1,5 +1,13 @@
 from pydantic_ai import Agent
 from schemas import Insight
+from pydantic_ai.models.openai import OpenAIModel
+from config import OPENROUTER_API_KEY, MODEL_NAME, BASE_URL
+
+model = OpenAIModel(
+    model_name=MODEL_NAME,
+    api_key=OPENROUTER_API_KEY,
+    base_url=BASE_URL,
+)
 
 
 agent = Agent(
