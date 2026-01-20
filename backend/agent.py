960631cfb13471462aaee7406ae78d4c48012eef
PDPSnG007 <pdpsng@googlemail.com>
2026-01-20 13:50:41 +0530

    Update agent.py

--- backend/agent.py
+++ backend/agent.py
@@ -3,12 +3,11 @@ from schemas import Insight
 from pydantic_ai.models.openai import OpenAIModel
 from config import OPENROUTER_API_KEY, MODEL_NAME, BASE_URL
 
+
 model = OpenAIModel(
     model_name=MODEL_NAME,
-    api_key=OPENROUTER_API_KEY,
-    base_url=BASE_URL,
+    base_url=BASE_URL,   # ✅ allowed
 )
-
 
 agent = Agent(
     model="openrouter:mistralai/mistral-7b-instruct",
